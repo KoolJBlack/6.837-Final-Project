e5fa048b36281cf7b83f9aa3452c29583b0833e3
Kaiying Liao <kygliao@gmail.com>
2013-12-04 23:40:53 -0500

    actually fixed views

--- two/src/Views.cpp
+++ two/src/Views.cpp
@@ -24,13 +24,13 @@ void Views::calculate_weights(Vector3f cam_center){
 			Vector3f projDir = ps[j]->dirToProjection(vs[i]);
 			Vector3f camDir = (cam_center - vs[i]).normalized();
 			float w = Vector3f::dot(projDir, camDir);
-			weights.push_back(w); // not dealing with occlusion right now
+			weights.push_back(max(0,w)); // not dealing with occlusion right now
 		}
 
 		//figure out the three closest projections according to their weights
-		int maxind[3] = {0,0,0};
+		int maxind[3] = {0,0,0}; 
 		for (unsigned int j = 1; j < ps.size(); j++){
-			if (weights[j] > weights[maxind[0]]){
+			if (weights[j] >= weights[maxind[0]]){
 				//reset the weight first
 				weights[maxind[2]] = 0.0f;				
 				// shift the indices
@@ -38,25 +38,35 @@ void Views::calculate_weights(Vector3f cam_center){
 				maxind[1] = maxind[0];
 				maxind[0] = j;				
 			}
-			else if (weights[j] > weights[maxind[1]]){	
+			else if (weights[j] >= weights[maxind[1]]){	
 				weights[maxind[2]] = 0.0f;			
 				maxind[2] = maxind[1];
 				maxind[1] = j;
 			}
-			else if (weights[j] > weights[maxind[2]]) {
+			else if (weights[j] >= weights[maxind[2]]) {
 				weights[maxind[2]] = 0.0f;	
 				maxind[2] = j;
 			}
-			else {
+			else if (maxind[0] == maxind[1]){
+				maxind[1] = j;
+				maxind[2] = j;
+			}
+			else if (maxind[1] == maxind[2]){
+				maxind[2] = j;
+			}
+			else 
+			{
 				// weight is too small, not one of the closest, set to 0
 				weights[j] = 0.0f;
 			}
-		}
+		} // end for each projection
+
 		float total_weight = weights[maxind[0]] + weights[maxind[1]] + weights[maxind[2]];
 		// normalize the weights. 
 		weights[maxind[0]] = weights[maxind[0]] / total_weight;
 		weights[maxind[1]] = weights[maxind[1]] / total_weight;
 		weights[maxind[2]] = weights[maxind[2]] / total_weight;
 		v_weights.push_back(weights);
-	}
+		cout << "weights = " << endl;
+	} // end for each vertex
 }
